c40a8da5067c102cc84c943fd5c2945730b7dc79
Cristian Cali <cristianmcali@gmail.com>
2023-07-10 22:33:57 -0300

    pantalla de registro 70%

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:buscar_app/presentation/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:buscar_app/tema_app.dart';
+import 'package:buscar_app/presentation/config/tema_app.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -18,17 +18,26 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor:const Color(0xFF010101),
         appBarTheme: const AppBarTheme(
           iconTheme: IconThemeData(size:40, color: Colors.black),
+          titleTextStyle: TextStyle(fontWeight: FontWeight.bold, color: Colors.black, fontSize: 23)
         ),
         colorScheme: const ColorScheme.highContrastDark(
           primary: Color(0xFFFFF81F),
-          secondary:Color(0xFFFFF81F), 
-          error: Color.fromARGB(253, 255, 0, 64), 
+          secondary: Color(0xFFFFF81F),
+          error: Color.fromARGB(252, 255, 71, 117), 
           background: Color(0xFF010101),
           onBackground: Color(0xFFFFF81F),
           surface: Color(0xFFFFF81F),
           onSurface: Color(0xFF010101),
-
-          )
+          tertiary: Color.fromARGB(255, 255, 255, 255),
+          inversePrimary: Color(0xFF66fff9)
+          ),
+          inputDecorationTheme: const InputDecorationTheme(
+            labelStyle: TextStyle(color: Color(0xFFFFF81F), fontSize: 20),
+            floatingLabelStyle: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+            color: Color(0xFFFFF81F))
+          ),
       ),
       //theme: const TemaApp(colorElegido: 0).createTheme(),     
       home: const SplashScreen()
