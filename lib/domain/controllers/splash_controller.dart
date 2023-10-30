5d9ed246974e6f5b8ea864ddac3c319ba333dae7
Cristian Cali <cristianmcali@gmail.com>
2023-10-29 21:35:19 -0300

    pre v1.0

--- lib/domain/controllers/splash_controller.dart
+++ lib/domain/controllers/splash_controller.dart
@@ -1,11 +1,17 @@
+import 'dart:convert';
+
 import 'package:buscar_app/presentation/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../infrastructure/conector_backend.dart';
 import '../../infrastructure/respuesta.dart';
+import '../../presentation/screens/home_screen.dart';
+import '../forms/login_form.dart';
 
 class SplashController extends GetxController {
+  get csrfTokenAndSessionController => null;
 
   Future<void> fetchCsrfToken() async {
     final respuesta =
@@ -13,8 +19,16 @@ class SplashController extends GetxController {
             .getCsrfToken();
 
     if (respuesta.estado == EstadoRespuesta.finalizadaOk) {
-      Get.off(() =>
-          const LoginScreen()); // Cambia '/nextScreen' por la ruta de la siguiente pantalla
+        LecturaCredenciales credenciales = LecturaCredenciales();
+        credenciales.leerCredenciales();
+      if (!credenciales.resultadoOk) {
+        Get.off(() => const LoginScreen());
+      } else {
+        Map<String, String> jsonRegistro = LoginForm(usuario: credenciales.correo, contrasenia: credenciales.contrasenia).aMap();
+        final respuestaLogin = await ConectorBackend(
+                ruta: 'login_flutter', method: HttpMethod.post, body: jsonRegistro).hacerRequest();
+        manejarRespuestaLogin(respuestaLogin);
+      }
     } else {
       String mensajeTitulo = 'error';
       String mensajeContenido = 'error';
@@ -46,5 +60,38 @@ class SplashController extends GetxController {
         ),
       );
     }
+  }
+
+  void manejarRespuestaLogin(Respuesta respuesta) {
+    if (respuesta.estado == EstadoRespuesta.finalizadaOk) {
+      final body = json.decode(respuesta.respuestaExistente!.body);
+      final String token = body['token'];
+      final String session = body['session'];
+      csrfTokenAndSessionController.setCsrfToken(token);
+      csrfTokenAndSessionController.setSessionId(session);
+      Get.off(() => const HomeScreen());
+    } else {
+      Get.off(() => const LoginScreen());
+    }
+  }
+}
+
+class LecturaCredenciales {
+  bool resultadoOk = false;
+  String correo = '';
+  String contrasenia = '';
+
+  LecturaCredenciales();
+  void leerCredenciales() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String? correoLeido = prefs.getString('correo');
+    final String? contraseniaLeida = prefs.getString('contrasenia');
+
+    if (correoLeido != null && contraseniaLeida != null) {
+      resultadoOk = true;
+      correo = correoLeido;
+      contrasenia = contraseniaLeida;
+    }
+
   }
 }
